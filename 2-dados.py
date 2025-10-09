3f2e2e9f4406e2af0276a352c64aa9947d24cbf7
ViitoDev <joaovictorpiresdev@gmail.com>
2025-10-09 20:39:08 -0300

    Fazendo estudo dos tipos de dados.

---
+++ 2-dados.py
@@ -0,0 +1,13 @@
+# PythonFlix
+
+name = "Top Gun Maverick"
+yearLaunch = 2023 
+noteMovie = 9.5
+PlanIncluded = False
+
+print(name)
+print(yearLaunch)
+print(type(name))
+print(type(yearLaunch))
+print(type(noteMovie))
+print(type(PlanIncluded))
