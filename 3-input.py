c96a8c0a37fdcb7c19dcd4776398357e3e0f40bd
ViitoDev <joaovictorpiresdev@gmail.com>
2025-10-09 20:47:05 -0300

    Fazendo estudo do input

---
+++ 3-input.py
@@ -0,0 +1,9 @@
+# Utilizando o input
+
+name = input("Digite o nome do filme: \n")
+yearLaunch = input("Digite o ano de lançamento do filme: \n")
+noteMovie = input("Digite a nota do filme: \n")
+
+print(name)
+print(yearLaunch)
+print(noteMovie)
